e7d32e24701534a58b48c567aa6a4c12a3348c6a
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-20 18:01:51 +0300

    feat: install antd & node-sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Controls from './components/Controls';
+
 function App() {
-  return <div className="app">App</div>;
+  return <div className="app">
+    <Controls />
+  </div>;
 }
 
 export default App;
