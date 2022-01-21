4449f293754e978abc6979cd9ba1049da490654e
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2022-01-21 15:47:53 +0300

    feat: response data from socket

--- server/src/wss.ts
+++ server/src/wss.ts
@@ -2,9 +2,16 @@ import { Server } from 'http';
 import WebSocket from 'ws';
 
 import * as ConnectionService from './resources/connections/connection.service';
+import * as MessageService from './resources/messages/message.service';
+import { SocketTypes } from './constants/SocketTypes';
 
 interface IWebSocketExtend extends WebSocket {
   sessionId: string;
+}
+
+interface IWebSocketData<T> {
+  type: SocketTypes;
+  payload?: T;
 }
 
 export const connectWS = async (server: Server) => {
@@ -14,16 +21,31 @@ export const connectWS = async (server: Server) => {
     const { id: sessionId } = await ConnectionService.create({});
     ws.sessionId = sessionId;
 
-    ws.on('message', (message: string) => {
-      const parsedMessage = JSON.parse(message);
-      console.log(parsedMessage);
+    ws.on('message', async (message: string) => {
+      const { type, payload } = JSON.parse(message) as IWebSocketData<string>;
+
+      switch (type) {
+        case SocketTypes.MESSAGE:
+          if (!payload) break;
+          await MessageService.create({ session: ws.sessionId, body: payload });
+          break;
+        case SocketTypes.ALL_ACTIVE_CONNECTIONS:
+          const response = JSON.stringify({
+            type: SocketTypes.ALL_ACTIVE_CONNECTIONS,
+            payload: Array.from(wss.clients).length,
+          });
+          ws.send(response);
+          break;
+        default:
+          console.log('Socket type is not defined!');
+      }
     });
 
     ws.on('close', async () => {
       await ConnectionService.deleteById(ws.sessionId);
     });
 
-    ws.send(`Session #${ws.sessionId} connected!`);
+    ws.send(JSON.stringify({ type: SocketTypes.CONNECTION, payload: ws.sessionId }));
   });
 
   return wss;
